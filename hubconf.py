fd211d2ca5d09c757ec441235bf85795e46efa8f
lyuwenyu <wenyu.lyu@gmail.com>
2021-04-19 17:26:10 +0800

    add hubconf

---
+++ hubconf.py
@@ -0,0 +1,16 @@
+dependencies = ['paddle']
+
+import paddle
+from model import MM as _MM
+
+
+def MM(pretrained=False):
+    '''This is a test demo for paddle hub
+    '''
+    mm = _MM()
+    if pretrained:
+        url = 'https://github.com/lyuwenyu/paddlehub_demo/releases/tag/v1.0/params.pd'
+        path = paddle.utils.download.get_weights_path_from_url(url)
+        mm.set_state_dict(paddle.load(path))
+    return mm
+    
