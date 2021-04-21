cc4b90b4ab0e145c9d890d3c7f13c1a488307c92
Wenyu <wenyu.lyu@gmail.com>
2021-04-21 14:15:09 +0800

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -4,10 +4,10 @@ import paddle
 from model import MM as _MM
 
 
-def MM(pretrained=False):
+def MM(out_channels=8, pretrained=False):
     '''This is a test demo for paddle hub
     '''
-    mm = _MM()
+    mm = _MM(out_channels)
     if pretrained:
         url = 'https://github.com/lyuwenyu/paddlehub_demo/releases/download/v1.0/params.pd'
         path = paddle.utils.download.get_weights_path_from_url(url)
