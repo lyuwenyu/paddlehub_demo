9c8718e457a3b2d83f303419bf9f8adec46fb6ae
Wenyu <wenyu.lyu@gmail.com>
2021-04-21 14:10:28 +0800

    Update model.py

--- model.py
+++ model.py
@@ -3,9 +3,9 @@ import paddle.nn as nn
 import paddle.nn.functional as F
 
 class MM(nn.Layer):
-  def __init__(self, ):
+  def __init__(self, out_channels):
       super(MM, self).__init__()
-      self.conv = nn.Conv2D(3, 8, 3, 2, 1)
+      self.conv = nn.Conv2D(3, out_channels, 3, 2, 1)
 
   def forward(self, x):
     out = self.conv(x)
